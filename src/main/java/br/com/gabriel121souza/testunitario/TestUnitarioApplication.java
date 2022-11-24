49221c9b2e477dfce38d406fa7cf616df8e3baba
gabriel121souza <gabriel121souza@gmail.com>
2022-11-24 14:49:16 -0300

    first commit

---
+++ src/main/java/br/com/gabriel121souza/testunitario/TestUnitarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel121souza.testunitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUnitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUnitarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel121souza/testunitario/TestUnitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel121souza.testunitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUnitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
